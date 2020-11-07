5897d83581a7ec87fe0f095b8f413cb0af6ba433
Daniel Armas <daniel62armas@gmail.com>
2020-11-06 18:30:15 -0600

    Start Scheme

--- src/schema.js
+++ src/schema.js
@@ -1,13 +1,27 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type Tasks {
-        id: String!
+    type Task {
+        id: Int!
         title: String!
         completed: Boolean!
     }
 
     type Query{
+        taskstodo(id: Int!): [Tasks!]!
+    }
+
+    type Mutation{
+        createTask(
+            id: Int!
+            title: String!
+            completed: Boolean!
+        ): Task!
+        editTask(
+            title: String!
+            completed: Boolean!
+        ): taskstodo!
+        deleteTask(id: Int!): Boolean!
     }
 `;
 
